2ca7c2d26e80f6cd40d51334431cb0e743ac1eb1
FaustoFang <51834130+FaustoFang@users.noreply.github.com>
2020-07-23 03:07:37 -0300

    Avance en el cuerpo principal.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./css/style.scss";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Info from "./components/Info";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Info />
     </div>
   );
 }
---
+++ src/components/Info.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Info = () => {
+    return (
+        <main>
+            <div className="titulo">
+                <h1>Que es Rudea?</h1>
+                <text>Lorem ipsum dolor sit amet consectetur illum detectus.</text>
+            </div>
+            
+            <div className="items">
+                <div className="imagen"></div>
+                <div className="text">
+                    <h2>Lorem ipsum dolor sit amet consectetur illum detectus.</h2>
+                </div>    
+            </div>
+            
+            
+            
+            
+
+        
+        </main>
+    );
+};
+
+export default Info;
