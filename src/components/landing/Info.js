415cc34c14c67113f645a4ba119ea32ea9bebbf6
GonzaDDV <gonzalodiazdevivar@gmail.com>
2020-09-06 13:30:52 -0300

    :rocket: Cambios finales

--- src/components/landing/Info.js
+++ src/components/landing/Info.js
@@ -9,7 +9,12 @@ const Info = () => {
 
       <div className="titulo-info">
         <h1>¿Qué es RUIDEA?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur illum detectus.</p>
+        <p>
+          Es el Registro con el que las personas con DEA obtienen un documento
+          verificado, con el fin de que las administraciones y las autoridades
+          puedan tener en consideración la legislación vigente y las
+          circunstancias que concurren en la persona portadora del pasaporte.
+        </p>
       </div>
 
       <div>
@@ -19,11 +24,23 @@ const Info = () => {
           </div>
           <div className="container-texto">
             <h2>¿Cómo funciona?</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-              nulla rhoncus sapien fermentum diam. Ut duis libero in aliquam
-              lectus sagittis diam sit.
-            </p>
+            <div className="container-textos">
+              <p>
+                La persona con algún tipo de dificultad específica del
+                aprendizaje (discalculia, dislexia, disortografía, dispraxia y/o
+                tda-h) se inscribe en el Registro, aportando un documento que
+                avale dicha dificultad, así como, una identificación para poder
+                confirmar la validez de ambos documentos.
+              </p>
+              <p>
+                Una vez comprobada dicha validez, el sistema envía el pasaporte
+                digital a la dirección de correo electrónico facilitada.
+              </p>
+              <p>
+                Las autoridades e instituciones podrán verificar la validez del
+                documento a través de esta página web o mediante un código QR.
+              </p>
+            </div>
           </div>
         </div>
       </div>
