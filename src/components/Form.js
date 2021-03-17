53f17e96ca69c5145221fa7f2f79fc727c67f8e4
GonzaDDV <gonzalodiazdevivar@gmail.com>
2021-03-17 10:49:21 -0300

    :rocket: Modificar solicitud desde el admin, success al subir archivos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,169 +1,156 @@
 /*eslint-disable*/
-import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
-import Cookies from "universal-cookie";
-import { axiosInstance } from "../axios";
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import Cookies from 'universal-cookie';
+import { axiosInstance } from '../axios';
 
-import Paso1 from "./Paso1";
-import Paso2 from "./Paso2";
-import Paso3 from "./Paso3";
-import Paso4 from "./Paso4";
-import Paso5 from "./Paso5";
+import Paso1 from './Paso1';
+import Paso2 from './Paso2';
+import Paso3 from './Paso3';
+import Paso4 from './Paso4';
+import Paso5 from './Paso5';
 
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
-const Form = (props) => {
-  const cookies = new Cookies();
-  const [paso, setPaso] = useState(1);
-  const [formData, setFormData] = useState({});
+const Form = props => {
+	const cookies = new Cookies();
+	const [paso, setPaso] = useState(1);
+	const [formData, setFormData] = useState({});
 
-  const id = cookies.get("id");
+	const id = cookies.get('id');
 
-  useEffect(() => {
-    const fetchDataFromId = async () => {
-      if (id) {
-        const res = await axiosInstance.post(`/admin/solicitudes/${id}`);
-        let tempUsuario = res.data.usuario;
-        delete tempUsuario.password;
+	useEffect(() => {
+		const fetchDataFromId = async () => {
+			if (id) {
+				const res = await axiosInstance.post(`/admin/solicitudes/${id}`);
+				let tempUsuario = res.data.usuario;
+				delete tempUsuario.password;
 
-        setFormData(tempUsuario);
-      }
-    };
-    fetchDataFromId();
-  }, []);
+				setFormData(tempUsuario);
+			}
+		};
+		fetchDataFromId();
+	}, []);
 
-  const siguientePaso = () => {
-    setPaso(paso + 1);
-  };
+	const siguientePaso = () => {
+		setPaso(paso + 1);
+	};
 
-  const pasoAnterior = (newFormData) => {
-    setPaso(paso - 1);
-    if (newFormData) {
-      setFormData({
-        ...formData,
-        ...newFormData,
-      });
-    }
-  };
+	const pasoAnterior = newFormData => {
+		setPaso(paso - 1);
+		if (newFormData) {
+			setFormData({
+				...formData,
+				...newFormData,
+			});
+		}
+	};
 
-  const handleFormChange = (newFormData) => {
-    setFormData({
-      ...formData,
-      ...newFormData,
-    });
-  };
+	const handleFormChange = newFormData => {
+		setFormData({
+			...formData,
+			...newFormData,
+		});
+	};
 
-  const historyPush = (path) => {
-    props.history.push(path);
-  };
+	const historyPush = path => {
+		props.history.push(path);
+	};
 
-  const showCurrentStep = () => {
-    switch (paso) {
-      case 1:
-        return (
-          <Paso1
-            siguientePaso={siguientePaso}
-            handleFormChange={handleFormChange}
-            formData={formData}
-          />
-        );
-      case 2:
-        return (
-          <Paso2
-            siguientePaso={siguientePaso}
-            pasoAnterior={pasoAnterior}
-            handleFormChange={handleFormChange}
-            formData={formData}
-          />
-        );
-      case 3:
-        return (
-          <Paso3
-            siguientePaso={siguientePaso}
-            pasoAnterior={pasoAnterior}
-            handleFormChange={handleFormChange}
-            formData={formData}
-          />
-        );
-      case 4:
-        return (
-          <Paso4
-            siguientePaso={siguientePaso}
-            pasoAnterior={pasoAnterior}
-            handleFormChange={handleFormChange}
-            formData={formData}
-          />
-        );
-      case 5:
-        return (
-          <Paso5
-            pasoAnterior={pasoAnterior}
-            formData={formData}
-            historyPush={historyPush}
-          />
-        );
-    }
-  };
+	const showCurrentStep = () => {
+		switch (paso) {
+			case 1:
+				return <Paso1 siguientePaso={siguientePaso} handleFormChange={handleFormChange} formData={formData} />;
+			case 2:
+				return (
+					<Paso2
+						siguientePaso={siguientePaso}
+						pasoAnterior={pasoAnterior}
+						handleFormChange={handleFormChange}
+						formData={formData}
+					/>
+				);
+			case 3:
+				return (
+					<Paso3
+						siguientePaso={siguientePaso}
+						pasoAnterior={pasoAnterior}
+						handleFormChange={handleFormChange}
+						formData={formData}
+					/>
+				);
+			case 4:
+				return (
+					<Paso4
+						siguientePaso={siguientePaso}
+						pasoAnterior={pasoAnterior}
+						handleFormChange={handleFormChange}
+						formData={formData}
+					/>
+				);
+			case 5:
+				return <Paso5 pasoAnterior={pasoAnterior} formData={formData} historyPush={historyPush} />;
+		}
+	};
 
-  return (
-    <motion.div
-      className="container-form"
-      exit={{ transform: "translateX(100vw)" }}
-      animate={{ transform: "translateX(0vw)" }}
-      initial={{ transform: "translateX(100vw)" }}
-    >
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>
-          Inscribirse | RUIDEA - Registro Único Iberoamericano de Personas con
-          Dificultades Específicas del Aprendizaje
-        </title>
-      </Helmet>
-      <Link className="cross" to="/" role="button"></Link>
-      <div className="container-barra">
-        <div className="barra-lateral">
-          <div className={`barra-verde barra-verde-${paso}`}></div>
-          <div className="puntos">
-            <div
-              className="punto pointer"
-              onClick={() => {
-                if (paso > 1) setPaso(1);
-              }}
-              data-selected={paso >= 1 && "true"}
-            ></div>
-            <div
-              className="punto pointer"
-              onClick={() => {
-                if (paso > 2) setPaso(2);
-              }}
-              data-selected={paso >= 2 && "true"}
-            ></div>
-            <div
-              className="punto pointer"
-              onClick={() => {
-                if (paso > 3) setPaso(3);
-              }}
-              data-selected={paso >= 3 && "true"}
-            ></div>
-            <div
-              className="punto pointer"
-              onClick={() => {
-                if (paso > 4) setPaso(4);
-              }}
-              data-selected={paso >= 4 && "true"}
-            ></div>
-            <div className="punto" data-selected={paso >= 5 && "true"}></div>
-          </div>
-        </div>
-      </div>
+	return (
+		<motion.div
+			className='container-form'
+			exit={{ transform: 'translateX(100vw)' }}
+			animate={{ transform: 'translateX(0vw)' }}
+			initial={{ transform: 'translateX(100vw)' }}
+		>
+			<Helmet>
+				<meta charSet='utf-8' />
+				<title>
+					Inscribirse | RUIDEA - Registro Único Iberoamericano de Personas con Dificultades Específicas del Aprendizaje
+				</title>
+			</Helmet>
+			<Link className='cross' to='/' role='button'></Link>
+			<div className='container-barra'>
+				<div className='barra-lateral'>
+					<div className={`barra-verde barra-verde-${paso}`}></div>
+					<div className='puntos'>
+						<div
+							className='punto pointer'
+							onClick={() => {
+								if (paso > 1) setPaso(1);
+							}}
+							data-selected={paso >= 1 && 'true'}
+						></div>
+						<div
+							className='punto pointer'
+							onClick={() => {
+								if (paso > 2) setPaso(2);
+							}}
+							data-selected={paso >= 2 && 'true'}
+						></div>
+						<div
+							className='punto pointer'
+							onClick={() => {
+								if (paso > 3) setPaso(3);
+							}}
+							data-selected={paso >= 3 && 'true'}
+						></div>
+						<div
+							className='punto pointer'
+							onClick={() => {
+								if (paso > 4) setPaso(4);
+							}}
+							data-selected={paso >= 4 && 'true'}
+						></div>
+						<div className='punto' data-selected={paso >= 5 && 'true'}></div>
+					</div>
+				</div>
+			</div>
 
-      <div className="container-inscripcion">
-        <h1 className="titulo">Inscribirse</h1>
-        {showCurrentStep()}
-      </div>
-    </motion.div>
-  );
+			<div className='container-inscripcion'>
+				<h1 className='titulo'>Inscribirse</h1>
+				{showCurrentStep()}
+			</div>
+		</motion.div>
+	);
 };
 
 export default Form;
--- src/components/admin/AdminSolicitud.js
+++ src/components/admin/AdminSolicitud.js
@@ -1,225 +1,203 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { axiosInstance } from "../../axios";
-import Swal from "sweetalert2";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from 'react';
+import { axiosInstance } from '../../axios';
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom';
 
-import iconSuccess from "../../img/svg/success-icon.svg";
-import iconSuccessPNG from "../../img/png/success-icon.png";
-import iconFail from "../../img/svg/failure-icon.svg";
-import iconFailPNG from "../../img/png/failure-icon.png";
+import iconSuccess from '../../img/svg/success-icon.svg';
+import iconSuccessPNG from '../../img/png/success-icon.png';
+import iconFail from '../../img/svg/failure-icon.svg';
+import iconFailPNG from '../../img/png/failure-icon.png';
 
-import LoadingScreen from "../LoadingScreen";
+import LoadingScreen from '../LoadingScreen';
 
-const AdminSolicitud = (props) => {
-  const [user, setUser] = useState({});
+const AdminSolicitud = props => {
+	const [user, setUser] = useState({});
 
-  const postResultToAPI = async (
-    estado,
-    mensajeMedico = "",
-    mostrarPopup = true
-  ) => {
-    try {
-      await axiosInstance.post(`/admin/respuesta`, {
-        estado,
-        mensajeMedico,
-        emailUsuario: user.correoElectronico,
-      });
-      mostrarPopup &&
-        Swal.fire({
-          icon: "success",
-          title: "Enviado!",
-        }).then(() => {
-          props.history.push("/admin/solicitudes");
-        });
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Lo sentimos!",
-        text: "Hubo un error al mandar la respuesta.",
-      });
-    }
-  };
+	const postResultToAPI = async (estado, mensajeMedico = '', mostrarPopup = true) => {
+		try {
+			await axiosInstance.post(`/admin/respuesta`, {
+				estado,
+				mensajeMedico,
+				emailUsuario: user.correoElectronico,
+			});
+			mostrarPopup &&
+				Swal.fire({
+					icon: 'success',
+					title: 'Enviado!',
+				}).then(() => {
+					props.history.push('/admin/solicitudes');
+				});
+		} catch (error) {
+			Swal.fire({
+				icon: 'error',
+				title: 'Lo sentimos!',
+				text: 'Hubo un error al mandar la respuesta.',
+			});
+		}
+	};
 
-  const getUserFromAPI = useCallback(async () => {
-    try {
-      const res = await axiosInstance.post(
-        `/admin/solicitudes/${props.match.params.id}`
-      );
-      setUser(res.data.usuario);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [props.match.params.id]);
+	const getUserFromAPI = useCallback(async () => {
+		try {
+			const res = await axiosInstance.post(`/admin/solicitudes/${props.match.params.id}`);
+			setUser(res.data.usuario);
+		} catch (err) {
+			console.log(err);
+		}
+	}, [props.match.params.id]);
 
-  useEffect(() => {
-    getUserFromAPI();
-  }, [getUserFromAPI]);
+	useEffect(() => {
+		getUserFromAPI();
+	}, [getUserFromAPI]);
 
-  return (
-    <>
-      {Object.keys(user).length === 0 ? (
-        <LoadingScreen />
-      ) : (
-        <main className="main-solicitud">
-          <Link className="cross" to="/admin/solicitudes" role="button"></Link>
-          <div className="container-solicitud">
-            <h2>Verificar solicitud</h2>
-            <p className="campo-informacion">
-              Nombre completo:{" "}
-              <span className="txt-info-usuario">
-                {user.nombre} {user.apellidos}
-              </span>
-            </p>
-            <p className="campo-informacion">
-              DNI o Documento:{" "}
-              <span className="txt-info-usuario">{user.numeroDocumento}</span>
-            </p>
-            <p className="campo-informacion">
-              Pais de residencia:{" "}
-              <span className="txt-info-usuario">{user.paisResidencia}</span>
-            </p>
-            <p className="campo-informacion">
-              Localidad:{" "}
-              <span className="txt-info-usuario">
-                {user.localidadResidencia}
-              </span>
-            </p>
-            <p className="campo-informacion">
-              Fecha de nacimiento:{" "}
-              <span className="txt-info-usuario">
-                {user.fechaNacimiento.substring(0, 10)}
-              </span>
-            </p>
-            <p className="campo-informacion">
-              Correo electrónico:{" "}
-              <span className="txt-info-usuario">{user.correoElectronico}</span>
-            </p>
-            <p className="campo-informacion">
-              Número de teléfono:{" "}
-              <span className="txt-info-usuario">{user.numeroTelefono}</span>
-            </p>
-            <p className="campo-informacion">
-              Diagnóstico:{" "}
-              <span className="txt-info-usuario">
-                {Object.keys(user.diagnostico)
-                  .filter((k) => user.diagnostico[k])
-                  .map((item, i) => (
-                    <span className="tipo-diagnostico">{`${
-                      item === "tdah" ? "TDA-H" : item
-                    }${
-                      i !==
-                      Object.keys(user.diagnostico).filter(
-                        (k) => user.diagnostico[k]
-                      ).length -
-                        1
-                        ? ", "
-                        : "."
-                    }`}</span>
-                  ))}
-              </span>
-            </p>
-            <p className="campo-informacion">
-              Link a Diagnóstico y DNI o Pasaporte:{" "}
-              <span className="txt-info-usuario">
-                <button
-                  className="btn-descargar-archivos"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => {
-                    const fetchLink = async (btn) => {
-                      const res = await axiosInstance.get(
-                        `/inscripcion/link-archivos/${user._id}`
-                      );
-                      const link = document.createElement("a");
-                      link.href = res.data;
-                      link.click();
-                    };
-                    fetchLink(e);
-                  }}
-                >
-                  Descargar
-                </button>
-              </span>
-            </p>
-          </div>
-          {user.estado === "pendiente" && (
-            <form>
-              <div className="botones-solicitud">
-                <button
-                  className="btn-aceptar-solicitud"
-                  name="aceptado"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    postResultToAPI("aceptado");
-                  }}
-                >
-                  Aceptar solicitud
-                </button>
-                <button
-                  className="btn-revision-solicitud"
-                  name="revision"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    const showDialog = async () => {
-                      await Swal.fire({
-                        input: "textarea",
-                        inputPlaceholder: "Enviar un mensaje al usuario",
-                        inputAttributes: {
-                          "aria-label": "Enviar un mensaje al usuario",
-                          style: "resize:none;",
-                          onChange: "",
-                        },
-                        showCancelButton: true,
-                      }).then((result) => {
-                        console.log(result.value);
-                        postResultToAPI(
-                          "revision",
-                          result.value,
-                          !result.value ? true : false
-                        );
-                      });
-                    };
-                    showDialog();
-                  }}
-                >
-                  Pedir revisión de solicitud
-                </button>
-                <button
-                  className="btn-rechazar-solicitud"
-                  name="rechazado"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    postResultToAPI("rechazado");
-                  }}
-                >
-                  Rechazar solicitud
-                </button>
-              </div>
-            </form>
-          )}
-          {user.estado === "aceptado" && (
-            <div className="estado-solicitud">
-              <picture>
-                <source srcSet={iconSuccess} type="image/svg+xml" />
-                <img src={iconSuccessPNG} alt="Ícono Solicitud Aprobada" />
-              </picture>
-              <span className="txt-aprobada">Aprobada!</span>
-            </div>
-          )}
+	return (
+		<>
+			{Object.keys(user).length === 0 ? (
+				<LoadingScreen />
+			) : (
+				<main className='main-solicitud'>
+					<Link className='cross' to='/admin/solicitudes' role='button'></Link>
+					<div className='container-solicitud'>
+						<h2>Verificar solicitud</h2>
+						<p className='campo-informacion'>
+							Nombre completo:{' '}
+							<span className='txt-info-usuario'>
+								{user.nombre} {user.apellidos}
+							</span>
+						</p>
+						<p className='campo-informacion'>
+							DNI o Documento: <span className='txt-info-usuario'>{user.numeroDocumento}</span>
+						</p>
+						<p className='campo-informacion'>
+							Pais de residencia: <span className='txt-info-usuario'>{user.paisResidencia}</span>
+						</p>
+						<p className='campo-informacion'>
+							Localidad: <span className='txt-info-usuario'>{user.localidadResidencia}</span>
+						</p>
+						<p className='campo-informacion'>
+							Fecha de nacimiento: <span className='txt-info-usuario'>{user.fechaNacimiento.substring(0, 10)}</span>
+						</p>
+						<p className='campo-informacion'>
+							Correo electrónico: <span className='txt-info-usuario'>{user.correoElectronico}</span>
+						</p>
+						<p className='campo-informacion'>
+							Número de teléfono: <span className='txt-info-usuario'>{user.numeroTelefono}</span>
+						</p>
+						<p className='campo-informacion'>
+							Diagnóstico:{' '}
+							<span className='txt-info-usuario'>
+								{Object.keys(user.diagnostico)
+									.filter(k => user.diagnostico[k])
+									.map((item, i) => (
+										<span className='tipo-diagnostico'>{`${item === 'tdah' ? 'TDA-H' : item}${
+											i !== Object.keys(user.diagnostico).filter(k => user.diagnostico[k]).length - 1 ? ', ' : '.'
+										}`}</span>
+									))}
+							</span>
+						</p>
+						<p className='campo-informacion'>
+							Link a Diagnóstico y DNI o Pasaporte:{' '}
+							<span className='txt-info-usuario'>
+								<button
+									className='btn-descargar-archivos'
+									target='_blank'
+									rel='noopener noreferrer'
+									onClick={e => {
+										const fetchLink = async btn => {
+											const res = await axiosInstance.get(`/inscripcion/link-archivos/${user._id}`);
+											const link = document.createElement('a');
+											link.href = res.data;
+											link.click();
+										};
+										fetchLink(e);
+									}}
+								>
+									Descargar
+								</button>
+							</span>
+						</p>
+					</div>
+					{user.estado === 'pendiente' && (
+						<form>
+							<div className='botones-solicitud'>
+								<button
+									className='btn-aceptar-solicitud'
+									name='aceptado'
+									onClick={e => {
+										e.preventDefault();
+										postResultToAPI('aceptado');
+									}}
+								>
+									Aceptar solicitud
+								</button>
+								<Link
+									to={`/solicitudes/${props.match.params.id}/modify`}
+									className='btn-revision-solicitud'
+									name='aceptado'
+									onClick={e => {}}
+								>
+									Modificar solicitud
+								</Link>
+								<button
+									className='btn-revision-solicitud'
+									name='revision'
+									onClick={e => {
+										e.preventDefault();
+										const showDialog = async () => {
+											await Swal.fire({
+												input: 'textarea',
+												inputPlaceholder: 'Enviar un mensaje al usuario',
+												inputAttributes: {
+													'aria-label': 'Enviar un mensaje al usuario',
+													style: 'resize:none;',
+													onChange: '',
+												},
+												showCancelButton: true,
+											}).then(result => {
+												console.log(result.value);
+												postResultToAPI('revision', result.value, !result.value ? true : false);
+											});
+										};
+										showDialog();
+									}}
+								>
+									Pedir revisión de solicitud
+								</button>
+								<button
+									className='btn-rechazar-solicitud'
+									name='rechazado'
+									onClick={e => {
+										e.preventDefault();
+										postResultToAPI('rechazado');
+									}}
+								>
+									Rechazar solicitud
+								</button>
+							</div>
+						</form>
+					)}
+					{user.estado === 'aceptado' && (
+						<div className='estado-solicitud'>
+							<picture>
+								<source srcSet={iconSuccess} type='image/svg+xml' />
+								<img src={iconSuccessPNG} alt='Ícono Solicitud Aprobada' />
+							</picture>
+							<span className='txt-aprobada'>Aprobada!</span>
+						</div>
+					)}
 
-          {user.estado === "rechazado" && (
-            <div className="estado-solicitud">
-              <picture>
-                <source srcSet={iconFail} type="image/svg+xml" />
-                <img src={iconFailPNG} alt="Ícono Solicitud Rechazada" />
-              </picture>
-              <span className="txt-rechazado">Rechazada</span>
-            </div>
-          )}
-        </main>
-      )}
-    </>
-  );
+					{user.estado === 'rechazado' && (
+						<div className='estado-solicitud'>
+							<picture>
+								<source srcSet={iconFail} type='image/svg+xml' />
+								<img src={iconFailPNG} alt='Ícono Solicitud Rechazada' />
+							</picture>
+							<span className='txt-rechazado'>Rechazada</span>
+						</div>
+					)}
+				</main>
+			)}
+		</>
+	);
 };
 
 export default AdminSolicitud;
