03e1189c1e545bb153c454603d0917972892e84b
GonzaDDV <53922815+GonzaDDV@users.noreply.github.com>
2021-04-20 08:20:19 -0300

    :memo: Cambio en los textos de ¿Qué es RUIDEA?

--- src/components/landing/Info.js
+++ src/components/landing/Info.js
@@ -27,7 +27,28 @@ const Info = () => {
 					</div>
 					<div className='container-texto'>
 						<h2>¿Cómo funciona?</h2>
-						<div className='container-textos'>
+							<div className="container-textos">
+						      <p>
+							La persona con algún tipo de dificultad específica del
+							aprendizaje (discalculia, dislexia, disortografía, dispraxia y/o
+							tda-h) se inscribe en el Registro, aportando un documento que
+							avale dicha dificultad, así como, una identificación para poder
+							confirmar la validez de ambos documentos.
+						      </p>
+						      <p>
+							Una vez comprobada dicha validez, el sistema envía el pasaporte
+							digital a la dirección de correo electrónico facilitada.
+						      </p>
+						      <p>
+							Las autoridades e instituciones podrán verificar la validez del
+							documento a través de esta página web o mediante un código QR.
+						      </p>
+						    </div>
+					</div>
+				</div>
+			</div>
+		
+			<div className="titulo-info">
 							<p>
 								Dicho documento es emitido por la Organización Iberoamericana DISFAM, como único representante del
 								colectivo en la gran mayoría de países Iberoamericanos.
@@ -37,9 +58,6 @@ const Info = () => {
 								Ministerios de Educación, brindaron su apoyo unánime en la Comisión Iberoamericana de Dislexia y otras
 								DEAs, celebrada el día 13 de noviembre de 2020.
 							</p>
-						</div>
-					</div>
-				</div>
 			</div>
 		</section>
 	);
